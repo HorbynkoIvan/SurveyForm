1f3f50f711c9d01c62b2bffa2beaec406cfe3179
Ivan <horbynko.i@gmail.com>
2018-06-17 18:12:36 +0300

    processed input: text, email, number, checkbox

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 var form = document.querySelector('#survey-form');
-var name = form.querySelector('#name');
+
+var yourName = form.querySelector('#name');
 var email = form.querySelector('#email');
 var age = form.querySelector('#number');
 var select = form.querySelector('#dropdown');
-var radio = form.querySelector();
-var checkbox;
 var comments = form.querySelector('#comments');
 
 var fields = form.querySelectorAll('.field');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log('name ' + name.value);
+    console.log('name ' + yourName.value);
     console.log('email ' + email.value);
     console.log('age ' + age.value);
     console.log('select ' + select.value);
-    console.log('radio ' + radio.value);
-     console.log('checkbox ' + radio.value);
+
+
+    /*start checkbox*/
+    var checked_checkboxes = form.querySelectorAll(
+        "input[name='kindofsport']:checked"
+    );
+    var checkboxes = [];
+    for (var q = 0; q < checked_checkboxes.length; q++) {
+        checkboxes.push(" " + checked_checkboxes[q].value);
+        checked_checkboxes[q].parentElement.style.color = 'red';//change colors of checked elements
+    }
+    console.log('checkboxes' + checkboxes)
+    /*finish checkbox*/
+
     console.log('comments ' + comments.value);
 
+
     var errors = form.querySelectorAll('.error');
-    for(var j = 0; j<errors.length; j++){
+    for (var j = 0; j < errors.length; j++) {
         errors[j].remove();
     }
 
-    for (var i = 0; i < fields.length; i++) {
-        if (!fields[i].value) {
-            console.log('field is empty' + fields[i]);
-            var errore = form.createElement('div');
-            errore.addClass = 'errore';
-            errore.innerHTML = 'empty field';
-            errore.style.color = 'red';
-            fields[i].parentElement.insertBefore(errore, fields[i]);
-        }
-    }
+    /*    for (var i = 0; i < fields.length; i++) {
+            if (!fields[i].value) {
+                console.log('field is empty' + fields[i]);
+                var errore = form.createElement('div');
+                errore.addClass = 'errore';
+                errore.innerHTML = 'empty field';
+                errore.style.color = 'red';
+                fields[i].parentElement.insertBefore(errore, fields[i]);
+            }
+        }*/
 })
